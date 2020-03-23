95485e6e70c363a6828b2cde3802ee9baf6fd5db
Zaini Miftah <zainimiftah@Zainis-MacBook-Air.local>
2020-03-23 18:28:32 +0700

    create stocks table and model

---
+++ database/migrations/2020_03_23_112250_create_stocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->float('rate', 8, 2);
+            $table->string('category', 50);
+            $table->integer('quantity', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
