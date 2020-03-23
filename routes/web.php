0c31d496ac4951cf71aac80861407463dc4e4b7e
Zaini Miftah <zainimiftah@Zainis-MacBook-Air.local>
2020-03-23 18:35:17 +0700

    define stock routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+$router->get('/stocks', 'StockController@index');
+$router->post('/stocks', 'StockController@store');
+$router->get('/stocks/{id}', 'StockController@show');
+$router->put('/stocks/{id}', 'StockController@update');
